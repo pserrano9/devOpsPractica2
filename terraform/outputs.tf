690a3210685e88ca181378e0fe8cc2b4d92afb54
pserrano9 <93371826+pserrano9@users.noreply.github.com>
2023-06-14 12:13:57 +0200

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,7 +5,7 @@ output "resource_group_name" {
 
 # Mostramos la Ip Publica creada para conectarnos a la Maquina virtual
 output "public_ip_address" {
-  value = azurerm_linux_virtual_machine.my_terraform_vm.public_ip_address
+  value = azurerm_public_ip.myPublicIp1.id
 }
 
 # Mostramos la key del admin
