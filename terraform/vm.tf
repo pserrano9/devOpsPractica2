ad1eb65c670d0dfabf0140a6181d919ef9e0b8b5
pserrano9 <93371826+pserrano9@users.noreply.github.com>
2023-06-14 11:56:22 +0200

    Create vm.tf

---
+++ terraform/vm.tf
@@ -0,0 +1,37 @@
+# Creamos una maquina Virtual
+resource "azurerm_linux_virtual_machine" "myVM1" {
+    name                = "my-first-azure-vm"
+    resource_group_name = azurerm_resource_group.rg.name
+    location            = azurerm_resource_group.rg.location
+    size                = var.vm_size
+    admin_username      = "adminUsername"
+    network_interface_ids = [ azurerm_network_interface.myNic1.id ]
+    disable_password_authentication = true
+
+    admin_ssh_key {
+        username = "adminUsername"
+        public_key = file("~/.ssh/id_rsa.pub")
+    }
+
+    os_disk {
+        caching = "ReadWrite"
+        storage_account_type = "Standard_LRS"
+    }
+
+    plan {
+        name      = "centos-8-stream-free"
+        product   = "centos-8-stream-free"
+        publisher = "cognosys"
+    }
+
+    source_image_reference {
+        publisher = "cognosys"
+        offer     = "centos-8-stream-free"
+        sku       = "centos-8-stream-free"
+        version   = "22.03.28"
+    }
+
+    boot_diagnostics {
+        storage_account_uri = azurerm_storage_account.stAccount.primary_blob_endpoint
+    }
+}
