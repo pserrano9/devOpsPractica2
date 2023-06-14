c94a7f6dc90abdb84fb550e4839c332bf2a1e615
pserrano9 <93371826+pserrano9@users.noreply.github.com>
2023-06-14 10:55:50 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,6 +21,6 @@ resource "azurerm_storage_account" "stAccount"{
   resource_group_name = azurerm_resource_group.rg.name
   location = azurerm_resource_group.rg.location
   account_tier = "Standard"
-  account_replication_Type = "LRS"
+  account_replication_type = "LRS"
  
   }
