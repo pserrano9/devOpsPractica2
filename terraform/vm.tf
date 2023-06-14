ea2423ac9ff0151c2980054e5217122afa51b360
pserrano9 <93371826+pserrano9@users.noreply.github.com>
2023-06-14 13:45:02 +0200

    Update vm.tf

--- terraform/vm.tf
+++ terraform/vm.tf
@@ -5,16 +5,16 @@ resource "azurerm_linux_virtual_machine" "myVM1" {
     location            = azurerm_resource_group.rg.location
     size                = "Standard_D1_v2"
     admin_username      = "adminUsername"
-    #admin_password      = "P@$$w0rd1234!"
+    admin_password      = "P@$$w0rd1234!"
     network_interface_ids = [ azurerm_network_interface.myNic1.id ]
-    #disable_password_authentication = false
-    disable_password_authentication = true
+    disable_password_authentication = false
+    #disable_password_authentication = true
    
-    admin_ssh_key {
-        username = "adminUsername"
+    #admin_ssh_key {
+        #username = "adminUsername"
         #public_key = file("~/.ssh/id_rsa.pub")
-        public_key = tls_private_key.example_ssh.public_key_openssh 
-    }
+        #public_key = tls_private_key.example_ssh.public_key_openssh 
+    #}
 
     os_disk {
         caching = "ReadWrite"
