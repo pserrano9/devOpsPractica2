fcf1bca6839caca76018aec3f2715e0cdd881a75
pserrano9 <93371826+pserrano9@users.noreply.github.com>
2023-06-14 11:44:42 +0200

    Update security.tf

--- terraform/security.tf
+++ terraform/security.tf
@@ -19,7 +19,7 @@ resource "azurerm_network_security_group" "mySecGroup" {
 
 # Vinculamos el Security Group con la interfaz de red
 resource "azurerm_network_interface_security_group_association" "mySecGroupAssociation1" {
-  network_interface_id = azurerm_network_interface_myNic1.id
+  network_interface_id = azurerm_network_interface.myNic1.id
   network_security_group_id = azurerm_network_security_group.mySecGroup.id
 }
   
