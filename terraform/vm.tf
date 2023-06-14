dffca80f02442f0ff0a180627048c6ec04fff46f
pserrano9 <93371826+pserrano9@users.noreply.github.com>
2023-06-14 14:37:43 +0200

    Update vm.tf

--- terraform/vm.tf
+++ terraform/vm.tf
@@ -13,7 +13,7 @@ resource "azurerm_linux_virtual_machine" "myVM1" {
     admin_ssh_key {
         username = "adminUsername"
         #public_key = file("~/.ssh/id_rsa.pub")
-        public_key = file("C:/.ssh/id_rsa.pub")
+        public_key = file("C:/Users/pserrano/.ssh/id_rsa.pub")
                
         #public_key = tls_private_key.example_ssh.public_key_openssh 
     }
