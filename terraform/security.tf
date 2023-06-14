9eeb741c6fed56d7e8695b4dc9a889d1d489889a
pserrano9 <93371826+pserrano9@users.noreply.github.com>
2023-06-14 11:40:49 +0200

    Create security.tf

---
+++ terraform/security.tf
@@ -0,0 +1,25 @@
+# Creamos un Security Group
+resource "azurerm_network_security_group" "mySecGroup" {
+  name = "sshtrafic"
+  location = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  
+  security_rule {
+    name = "SSH"
+    priority = 1001
+    direction = "Inbound"
+    access = "Allow"
+    protocol = "Tcp"
+    source_port_range = "*"
+    destination_port_range = "22"
+    source_address_prefix = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# Vinculamos el Security Group con la interfaz de red
+resource "azurerm_network_interface_security_group_association" "mySecGroupAssociation1" {
+  network_interface_id = azurerm_network_interface_myNic1.id
+  network_security_group_id = azurerm.network.security_group.mySecGroup.id
+}
+  
