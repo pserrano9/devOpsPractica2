2006913db94b27f345d26c372573934c1b6affac
pserrano9 <93371826+pserrano9@users.noreply.github.com>
2023-06-14 11:27:54 +0200

    Update network.tf

--- terraform/network.tf
+++ terraform/network.tf
@@ -11,7 +11,7 @@ resource "azurerm_subnet" "mySubnet" {
   name = "terraformsubnet"
   resource_group_name = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.myNet.name
-  address_prefixez = ["10.0.1.0/24"]
+  address_prefixes = ["10.0.1.0/24"]
 }
 
 # Creamos una Interfaz de Red
