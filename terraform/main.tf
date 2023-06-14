ef1278c0aa89b6b72a4ca7322cf71f153f09272a
pserrano9 <93371826+pserrano9@users.noreply.github.com>
2023-06-14 10:52:00 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,15 +8,6 @@ terraform {
   }
 }
 
-# Creamos el Servicio Principal e indicamos los datos para autentificar
-provider "azurerm" 
-  features {}
-  subscription_id = "c60a65ff-0234-44cc-abf5-cdca6ecf27d0"
-  client_id       = "995044c1-34c1-4868-9724-6ef339037471"
-  client_secret   = "nzQ8Q~S4f1joQ39MCMI6mo4y1pHT53LLttIw8aVz"
-  tenant_id       = "899789dc-202f-44b4-8472-a6d40f9eb440"
-
-
 # Creamos un grupo de recursos
 resource "azurerm_resource_group" "rg"{
   name = "kubernetes_rg"
