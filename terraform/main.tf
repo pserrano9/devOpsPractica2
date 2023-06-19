33b7d1e27d7222aee7b2c3ff23addc9998ec4c94
pserrano9 <93371826+pserrano9@users.noreply.github.com>
2023-06-19 19:16:43 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,10 +25,10 @@ resource "azurerm_storage_account" "stAccount"{
 
 # Creamos un Azure Container Registry
 resource "azurerm_container_registry" "acr" {
-  name = var.registry_name
+  name = "acr_devops"
   resource_group_name = azurerm_resource_group.rg.name
   location = azurerm_resource_group.rg.location
-  sku = var.registry_sku
+  sku = "Basic"
   admin_enabled = true
 }
 
