bbab1ffa676defb92cb5b6d5522c22c5370d2c0e
pserrano9 <93371826+pserrano9@users.noreply.github.com>
2023-06-22 19:19:52 +0200

    Update aks-cluster.tf

--- terraform/aks-cluster.tf
+++ terraform/aks-cluster.tf
@@ -1,21 +1,40 @@
-#resource "azurerm_kubernetes_cluster" "default" {
-  #name                = aksdevops"
-  #location            = azurerm_resource_group.default.location
- # resource_group_name = azurerm_resource_group.default.name
-  #dns_prefix          = "${random_pet.prefix.id}-k8s"
+# Generate random resource group name
+resource "random_pet" "rg_name" {
+  prefix = var.resource_group_name_prefix
+}
 
- # default_node_pool {
-  #  name            = "default"
-   # node_count      = 2
-   # vm_size         = "Standard_B2s"
-    #os_disk_size_gb = 30
-  #}
+resource "random_pet" "azurerm_kubernetes_cluster_name" {
+  prefix = "cluster"
+}
 
-#  service_principal {
- #   client_id     = var.appId
-  #  client_secret = var.password
-  #}
+resource "random_pet" "azurerm_kubernetes_cluster_dns_prefix" {
+  prefix = "dns"
+}
 
-#  role_based_access_control_enabled = true
+resource "azurerm_kubernetes_cluster" "k8s" {
+  location            = azurerm_resource_group.rg.location
+  name                = random_pet.azurerm_kubernetes_cluster_name.id
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = random_pet.azurerm_kubernetes_cluster_dns_prefix.id
 
-#}
+  identity {
+    type = "SystemAssigned"
+  }
+
+  default_node_pool {
+    name       = "agentpool"
+    vm_size    = "Standard_D2_v2"
+    node_count = var.node_count
+  }
+  linux_profile {
+    admin_username = "ubuntu"
+
+    ssh_key {
+      key_data = jsondecode(azapi_resource_action.ssh_public_key_gen.output).publicKey
+    }
+  }
+  network_profile {
+    network_plugin    = "kubenet"
+    load_balancer_sku = "standard"
+  }
+}
