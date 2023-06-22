0a7523e88697af5a4ec6cbc47486beaddebfd61d
pserrano9 <93371826+pserrano9@users.noreply.github.com>
2023-06-22 19:22:24 +0200

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -17,12 +17,41 @@ output "acr_admin_pass" {
   sensitive = true
 }
 
-# Mostramos la Ip Publica creada para conectarnos a la Maquina virtual
-#output "public_ip_address" {
- # value = azurerm_public_ip.myPublicIp1.id
-#}
+output "kubernetes_cluster_name" {
+  value = azurerm_kubernetes_cluster.k8s.name
+}
 
-# Mostramos la key del admin
- #output "tls_private_key" {
- #  value     = tls_private_key.example_ssh.public_key_openssh
- #}
+output "client_certificate" {
+  value     = azurerm_kubernetes_cluster.k8s.kube_config[0].client_certificate
+  sensitive = true
+}
+
+output "client_key" {
+  value     = azurerm_kubernetes_cluster.k8s.kube_config[0].client_key
+  sensitive = true
+}
+
+output "cluster_ca_certificate" {
+  value     = azurerm_kubernetes_cluster.k8s.kube_config[0].cluster_ca_certificate
+  sensitive = true
+}
+
+output "cluster_password" {
+  value     = azurerm_kubernetes_cluster.k8s.kube_config[0].password
+  sensitive = true
+}
+
+output "cluster_username" {
+  value     = azurerm_kubernetes_cluster.k8s.kube_config[0].username
+  sensitive = true
+}
+
+output "host" {
+  value     = azurerm_kubernetes_cluster.k8s.kube_config[0].host
+  sensitive = true
+}
+
+output "kube_config" {
+  value     = azurerm_kubernetes_cluster.k8s.kube_config_raw
+  sensitive = true
+}
