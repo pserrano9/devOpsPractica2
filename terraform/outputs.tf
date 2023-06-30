63b76dbebd2e758958570333c2eaadd78eeb93c1
pserrano9 <93371826+pserrano9@users.noreply.github.com>
2023-06-30 14:10:14 +0200

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,6 +3,12 @@ output "resource_group_name" {
   value = azurerm_resource_group.rg.name
 }
 
+# IP Publica de la VM
+output "VM-IP" {
+     description = "The VM Public IP is:"
+     value = azurerm_public_ip.vm1publicip.ip_address
+ }    
+
 output "acr_login_server" {
   value = azurerm_container_registry.acr.login_server
 }
@@ -17,41 +23,41 @@ output "acr_admin_pass" {
   sensitive = true
 }
 
-output "kubernetes_cluster_name" {
-  value = azurerm_kubernetes_cluster.k8s.name
-}
+#output "kubernetes_cluster_name" {
+#  value = azurerm_kubernetes_cluster.k8s.name
+#}
 
-output "client_certificate" {
-  value     = azurerm_kubernetes_cluster.k8s.kube_config[0].client_certificate
-  sensitive = true
-}
+#output "client_certificate" {
+ # value     = azurerm_kubernetes_cluster.k8s.kube_config[0].client_certificate
+ # sensitive = true
+#}
 
-output "client_key" {
-  value     = azurerm_kubernetes_cluster.k8s.kube_config[0].client_key
-  sensitive = true
-}
+#output "client_key" {
+ # value     = azurerm_kubernetes_cluster.k8s.kube_config[0].client_key
+ # sensitive = true
+#}
 
-output "cluster_ca_certificate" {
-  value     = azurerm_kubernetes_cluster.k8s.kube_config[0].cluster_ca_certificate
-  sensitive = true
-}
+#output "cluster_ca_certificate" {
+ # value     = azurerm_kubernetes_cluster.k8s.kube_config[0].cluster_ca_certificate
+  #sensitive = true
+#}
 
-output "cluster_password" {
-  value     = azurerm_kubernetes_cluster.k8s.kube_config[0].password
-  sensitive = true
-}
+#output "cluster_password" {
+#  value     = azurerm_kubernetes_cluster.k8s.kube_config[0].password
+ # sensitive = true
+#}
 
-output "cluster_username" {
-  value     = azurerm_kubernetes_cluster.k8s.kube_config[0].username
-  sensitive = true
-}
+#output "cluster_username" {
+#  value     = azurerm_kubernetes_cluster.k8s.kube_config[0].username
+ # sensitive = true
+#}
 
-output "host" {
-  value     = azurerm_kubernetes_cluster.k8s.kube_config[0].host
-  sensitive = true
-}
+#output "host" {
+ # value     = azurerm_kubernetes_cluster.k8s.kube_config[0].host
+#  sensitive = true
+#}
 
-output "kube_config" {
-  value     = azurerm_kubernetes_cluster.k8s.kube_config_raw
-  sensitive = true
-}
+#output "kube_config" {
+#  value     = azurerm_kubernetes_cluster.k8s.kube_config_raw
+#  sensitive = true
+#}
