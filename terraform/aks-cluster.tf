ed2ee9354d18b94fbaf6ab91ac91c9d92f45f4c4
pserrano9 <93371826+pserrano9@users.noreply.github.com>
2023-07-01 12:37:49 +0200

    Update aks-cluster.tf

--- terraform/aks-cluster.tf
+++ terraform/aks-cluster.tf
@@ -1,36 +1,37 @@
-#resource "azurerm_kubernetes_cluster" "k8s" {
-#  location            = azurerm_resource_group.rg.location
-#  name                = "cluster"
- # resource_group_name = azurerm_resource_group.rg.name
- # #dns_prefix          = random_pet.azurerm_kubernetes_cluster_dns_prefix.id
-#  dns_prefix          = "dns"
+resource "azurerm_kubernetes_cluster" "k8s" {
+ location            = azurerm_resource_group.rg.location
+ name                = "cluster"
+ resource_group_name = azurerm_resource_group.rg.name
+ dns_prefix          = "dns"
 
- # identity {
- #   type = "SystemAssigned"
- # }
+ identity {
+    type = "SystemAssigned"
+ }
 
-#  default_node_pool {
- #   name       = "agentpool"
- #   vm_size    = "Standard_D11_v2"
- #   node_count = 1
- # }
- # linux_profile {
-  #  admin_username = "ubuntu"
+ default_node_pool {
+    name       = "agentpool"
+    vm_size    = "Standard_D11_v2"
+    node_count = 1
+  }
+ 
+ linux_profile {
+    admin_username = "ubuntu"
 
- #   ssh_key {
-  #    key_data = jsondecode(azapi_resource_action.ssh_public_key_gen.output).publicKey
-  #  }
- # }
- # network_profile {
- #   network_plugin    = "kubenet"
-  #  load_balancer_sku = "standard"
- # }
-#}
+    ssh_key {
+     key_data = jsondecode(azapi_resource_action.ssh_public_key_gen.output).publicKey
+     }
+  }
+
+  network_profile {
+    network_plugin    = "kubenet"
+    load_balancer_sku = "standard"
+  }
+}
 
 # Conectar ACR con AKS
-#resource "azurerm_role_assignment" "example" {
- # principal_id                     = azurerm_kubernetes_cluster.k8s.kubelet_identity[0].object_id
- # role_definition_name             = "AcrPull"
- # scope                            = azurerm_container_registry.acr.id
- # skip_service_principal_aad_check = true
-#}
+ resource "azurerm_role_assignment" "example" {
+  principal_id                     = azurerm_kubernetes_cluster.k8s.kubelet_identity[0].object_id
+  role_definition_name             = "AcrPull"
+  scope                            = azurerm_container_registry.acr.id
+  skip_service_principal_aad_check = true
+}
