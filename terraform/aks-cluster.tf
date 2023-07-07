ce9199d8ebfa081ca91264ae67cc4cf4af26168b
pserrano9 <93371826+pserrano9@users.noreply.github.com>
2023-07-07 19:45:33 +0200

    Update aks-cluster.tf

--- terraform/aks-cluster.tf
+++ terraform/aks-cluster.tf
@@ -30,8 +30,8 @@ resource "azurerm_kubernetes_cluster" "k8s" {
 
  # Conectar ACR con AKS
  resource "azurerm_role_assignment" "example" {
-   principal_id                     = azurerm_kubernetes_cluster.k8s.kubelet_identity[0].object_id
-   role_definition_name             = "AcrPull"
    scope                            = azurerm_container_registry.acr.id
+   role_definition_name             = "AcrPull"
+   principal_id                     = azurerm_kubernetes_cluster.k8s.kubelet_identity[0].object_id
    skip_service_principal_aad_check = true
 }
