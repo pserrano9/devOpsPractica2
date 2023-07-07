2f1d7bba75d6414d720a4188de78e03b4e109fe0
pserrano9 <93371826+pserrano9@users.noreply.github.com>
2023-07-07 21:02:24 +0200

    Update acr.tf

--- terraform/acr.tf
+++ terraform/acr.tf
@@ -1,8 +1,8 @@
 # Creamos un Azure Container Registry
-#resource "azurerm_container_registry" "acr" {
- # name = "acrdevopsPablo"
- # resource_group_name = azurerm_resource_group.rg.name
- # location = azurerm_resource_group.rg.location
- # sku = "Basic"
-# admin_enabled = true
-#}
+resource "azurerm_container_registry" "acr" {
+  name = "acrdevopsPablo"
+  resource_group_name = azurerm_resource_group.rg.name
+  location = azurerm_resource_group.rg.location
+  sku = "Basic"
+ admin_enabled = true
+}
