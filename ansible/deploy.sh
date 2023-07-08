fa4415c5265c9c72eccfd1e39886761548028264
pserrano9 <93371826+pserrano9@users.noreply.github.com>
2023-07-08 10:31:25 +0200

    Update deploy.sh

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -2,4 +2,5 @@
 
 # añadir tantas líneas como sean necesarias para el correcto despligue
 ansible-playbook -i inventory 01_playbook_podman.yaml
-ansible-playbook -i inventory 02_playbook_aks.yaml
+ansible-playbook -i inventory 02_playbook_acr.yaml
+ansible-playbook -i inventory 03_playbook_aks.yaml
