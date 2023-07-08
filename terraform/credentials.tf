fd4fb616a88787fad54a9fc704bd2089ed40d916
pserrano9 <93371826+pserrano9@users.noreply.github.com>
2023-07-08 10:28:52 +0200

    Update credentials.tf

--- terraform/credentials.tf
+++ terraform/credentials.tf
@@ -1,7 +1,7 @@
 provider "azurerm" {
   features {}
   subscription_id = "c60a65ff-0234-44cc-abf5-cdca6ecf27d0"
-  client_id       = "995044c1-34c1-4868-9724-6ef339037471"
-  client_secret   = "nzQ8Q~S4f1joQ39MCMI6mo4y1pHT53LLttIw8aVz"
+  client_id       = "52b1b166-404a-4839-9ec6-d878215d5bd8"
+  client_secret   = "Xc98Q~2_eZj8uvigZ8sBqkdk8qQkJMaWkwy.5bfQ"
   tenant_id       = "899789dc-202f-44b4-8472-a6d40f9eb440"
 }
