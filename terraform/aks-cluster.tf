0294d6f792d9212368e4dd4eb901ed24a9d82517
pserrano9 <93371826+pserrano9@users.noreply.github.com>
2023-07-08 11:39:58 +0200

    Update aks-cluster.tf

--- terraform/aks-cluster.tf
+++ terraform/aks-cluster.tf
@@ -22,29 +22,29 @@ network_profile {
 }
 
  # Conectar ACR con AKS
-# resource "azurerm_role_assignment" "example" {
-  # scope                            = azurerm_container_registry.acr.id
- #  role_definition_name             = "AcrPull"
- #  principal_id                     = azurerm_kubernetes_cluster.k8s.kubelet_identity[0].object_id
- #  skip_service_principal_aad_check = true
+  resource "azurerm_role_assignment" "example" {
+   scope                            = azurerm_container_registry.acr.id
+   role_definition_name             = "AcrPull"
+   principal_id                     = azurerm_kubernetes_cluster.k8s.kubelet_identity[0].object_id
+   skip_service_principal_aad_check = true
+}
+
+#resource "azurerm_user_assigned_identity" "aks" {
+#  location            = azurerm_resource_group.rg.location
+#  name                = "aksIdentity"
+#  resource_group_name = azurerm_resource_group.rg.name
 #}
 
-resource "azurerm_user_assigned_identity" "aks" {
-  location            = azurerm_resource_group.rg.location
-  name                = "aksIdentity"
-  resource_group_name = azurerm_resource_group.rg.name
-}
+#resource "azurerm_role_assignment" "aks_network" {
+ # scope                = azurerm_resource_group.rg.id
+#  role_definition_name = "Network Contributor"
+#  principal_id         = azurerm_user_assigned_identity.aks.principal_id
+#}
 
-resource "azurerm_role_assignment" "aks_network" {
-  scope                = azurerm_resource_group.rg.id
-  role_definition_name = "Network Contributor"
-  principal_id         = azurerm_user_assigned_identity.aks.principal_id
-}
-
-resource "azurerm_role_assignment" "aks_acr" {
-  scope                = azurerm_container_registry.acr.id
-  role_definition_name = "AcrPull"
-  principal_id         = azurerm_user_assigned_identity.aks.principal_id
-}
+#resource "azurerm_role_assignment" "aks_acr" {
+ # scope                = azurerm_container_registry.acr.id
+ # role_definition_name = "AcrPull"
+ # principal_id         = azurerm_user_assigned_identity.aks.principal_id
+#}
 
 
